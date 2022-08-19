f8a066e810fc9a68c007d786b491118b404b262c
Pbaquero30 <111189188+Pbaquero30@users.noreply.github.com>
2022-08-19 18:26:50 -0500

    Modificacion para agregar mas informacion--Paola B

--- Presentacion.java
+++ Presentacion.java
@@ -1,6 +1,8 @@
 public class Presentacion {
     public static void main(String[] args) {
-        System.out.println("Hola Mi nombre es Paola!");
+        System.out.println("Hola Mi nombre es Paola Andrea Baquero");
+        System.out.println("Soy ingeniera electronica vivo en Villavicencio " +
+                "en este proyecto trabajere como desarrollador");
     }
 }
 
